08f8d1badbfe9bf48d1d0066ba8413197b388b54
Paulo Rodrigues <pjtsrodrigues1970@hotmail.com>
2015-05-20 15:00:05 +0100

    Create index.php
    Creating a php project

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>ACME Products Inc</title>
+		<link rel="stylesheet" type="text/css" href="http://paulorodrigues.atspace.cc/php/styles/main.css">
+	</head>
+	<body>
+		<h1>ACME Products Inc (In Progress)</h1>
+		<p>Would you like to view our products?</p>
+		<p>First, you need to create a customer profile.  Press the button below to create a customer profile.</p>
+		<form action="createCustomer.php" method="post">
+		<input type="submit" id="createCustomer" value="Create Customer Information"></input>			
+		</form>
+			</body>
+</html>
